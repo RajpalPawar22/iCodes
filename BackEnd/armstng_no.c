05478b5d1d713ccae4d7dc708d3f4c57a6211f63
rajpal.pawar <rajpalpawar77@gmail.com>
2025-05-27 17:21:11 +0530

    yo! first commit

--- BackEnd/armstng_no.c
+++ BackEnd/armstng_no.c
@@ -1,15 +1,28 @@
 #include<stdio.h>
+#include<math.h>
 int main()
 {
-    int a;
+    int a, digit=0, sum=0;
     printf("Enter a number: ");
     scanf("%d", &a);
-    int original = a, sum = 0, digit;  
-    while (a > 0) {
-        digit = a % 10; // Get the last digit
-        sum += digit * digit * digit; // Add the cube of the digit to sum
-        a /= 10; // Remove the last digit
+    int org=a, num_of_dig=floor(log10(a))+1;
+    while (a>0)
+    {
+        digit=a%10;
+        sum+=pow(digit, num_of_dig);
+        a/=10;
     }
-
+    if(sum==org)
+    {
+        printf("The no. %d is an armstrong number.\n", org);
+    }
+    else
+    {
+        printf("The no. %d is not an armstrong number.\n", org);
+    }
+    return 0;
 
 }
+
+
+
