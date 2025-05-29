a3074ce818b0d236388dce6d9e53d79891859ebd
rajpal.pawar <rajpalpawar77@gmail.com>
2025-05-29 08:34:37 +0530

    syncccc

--- BackEnd/calci.c
+++ BackEnd/calci.c
@@ -1,10 +1,12 @@
-#include<stdio.h> /*header file hai*/
+#include<stdio.h> 
 #include<math.h>
 int main(){
     int a, b;
-    printf("number dal:\n");
-    
-    scanf("%d %d", &a, &b);
-    int sum = pow(a, b);
-    printf("sum is %d", sum);
+    char op;
+    printf("Enter first number: ");
+    scanf("%d", &a);
+    printf("Enter second number: ");
+    scanf("%d", &b);
+    printf("Enter operator (+, -, *, /): ");
+    scanf(" %c", &op);
 }
