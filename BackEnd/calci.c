823162316d89c7b7b7cf5d5b3f5f34ef18eba5fa
rajpal.pawar <rajpalpawar77@gmail.com>
2025-05-29 09:37:12 +0530

    calci done

--- BackEnd/calci.c
+++ BackEnd/calci.c
@@ -7,6 +7,26 @@ int main(){
     scanf("%d", &a);
     printf("Enter second number: ");
     scanf("%d", &b);
-    printf("Enter operator (+, -, *, /): ");
+    printf("Enter operator (+, -, *, /, ^{for power}): ");
     scanf(" %c", &op);
-}
+    {
+        switch(op){
+            case '+':
+                printf("The sum of the number %d and %d is: %d", a, b, a+b);
+                break;
+            case '-':
+                printf("The subtraction of %d from %d is: %d", b, a, a-b);
+                break;
+            case '*':
+                printf("The multiplication of both the numbers %d and %d is: %d", a, b, a*b);
+                break;
+            case '/':
+                printf("The division of %d by %d is: %d", a, b, a/b);
+                break;
+            case '^':
+                printf("%d raised to the power of %d is: %lf", a, b, pow(a, b) );
+            break;
+        }
+    }
+    
+} 
