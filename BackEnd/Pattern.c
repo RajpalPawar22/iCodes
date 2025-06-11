b7da8180294cb638e9596f50a662298b4268b459
rajpal.pawar <rajpalpawar77@gmail.com>
2025-06-11 20:11:27 +0530

    R

--- BackEnd/Pattern.c
+++ BackEnd/Pattern.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
 int main(){
     int i,j, k;
-   for (i = 1; i<=5; i++) {
+   for (i = 0; i<=5; i++) {
        
-        for (j=5; j>=0; j++){
-        printf(" *");
+        for (j=0; j<=3; j++){
+            if((i==0 || i==2)||(i==1 && j!=1&& j!=2)||(i==3 && j%2==0)||(i==4 && j!=1 && j!=2)|| (i==5 && j!=1 && j!=2))
+        printf("*");
+            else printf(" ");
         }
         printf("\n");
     }
